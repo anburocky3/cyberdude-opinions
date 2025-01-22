a815dd13cf63de668555f72dfe41ac5850ea9721
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2025-01-23 00:49:43 +0530

    chore: view production data ondemand

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Suggestion;
+use Illuminate\Support\Facades\DB;
 
 class SiteController extends Controller
 {
     public function index()
     {
-        $suggestions = Suggestion::latest()->paginate(10);;
+        if (config('app.env') === 'local') {
+            $suggestions = Suggestion::on('production_mysql')->latest()->paginate(10);
+        } else {
+            $suggestions = Suggestion::latest()->paginate(10);
+        }
 
         return view('home', compact('suggestions'));
     }
