052208069cbb713d1ab278b1e7dedcf6ff78d674
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2025-01-25 01:45:54 +0530

    fix: navigate to admin via github

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,21 +16,43 @@ class AuthController extends Controller
     public function callback()
     {
         $githubUser = Socialite::driver('github')->user();
-        
-        // If the user doesn't exist create, else update the existing one
-        $user = User::updateOrCreate([
-            'github_id' => $githubUser->id,
-        ], [
-            'provider_id' => $githubUser->getId(),
-            'name' => $githubUser->getName() ?? $githubUser->getNickname(),
-            'email' => $githubUser->getEmail(),
-            'username' => $githubUser->getNickname(),
-            'avatar' => $githubUser->avatar,
-            'token' => $githubUser->token,
-            'refresh_token' => $githubUser->refreshToken,
-        ]);
 
-        Auth::login($user, true);
+        // Check if a user with the same email already exists
+        $existingUser = User::where('email', $githubUser->getEmail())->first();
+
+        if ($existingUser) {
+            // Update the user's information
+            $existingUser->update([
+                'provider_id' => $githubUser->getId(),
+                'name' => $githubUser->getName() ?? $githubUser->getNickname(),
+                'username' => $githubUser->getNickname(),
+                'avatar' => $githubUser->avatar,
+                'token' => $githubUser->token,
+                'refresh_token' => $githubUser->refreshToken,
+            ]);
+
+
+            // If the user exists, log them in without updating
+            Auth::login($existingUser, true);
+
+            if ($existingUser->role == 'admin') {
+                return redirect()->route('admin.index');
+            }
+        } else {
+            // If the user doesn't exist, create a new one
+            $user = User::updateOrCreate([
+                'github_id' => $githubUser->id,
+            ], [
+                'provider_id' => $githubUser->getId(),
+                'name' => $githubUser->getName() ?? $githubUser->getNickname(),
+                'username' => $githubUser->getNickname(),
+                'avatar' => $githubUser->avatar,
+                'token' => $githubUser->token,
+                'refresh_token' => $githubUser->refreshToken,
+            ]);
+
+            Auth::login($user, true);
+        }
 
         return redirect()->intended();
     }
